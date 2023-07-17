b08b3ae403090b995b406fbd35ced1cf43b5be81
lucadiogo <lucadiogo@usp.br>
2023-07-16 21:49:58 -0300

    update packages to fix nuxt version and create new pages

---
+++ pages/leaderboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-for="data in myJson">{{data}}</div>
+    </div>
+</template>
+
+
+<script>
+      import json from '../../galaxy-raiders-api/app/core/score/Leaderboard.json'
+      export default{
+          data(){
+              return{
+                  myJson: json
+              }
+          }
+      }
+</script>
+
